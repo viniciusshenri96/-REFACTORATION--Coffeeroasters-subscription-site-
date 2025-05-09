7bcbe42397868c3fa5b794c763174a44fa89155e
Lucas Guerra <125227136+eolkazin@users.noreply.github.com>
2025-05-08 22:34:41 -0300

    feat: Implementa autenticação e cadastro de usuários
    Implementa a lógica de autenticação e cadastro de usuários, incluindo:
    
    - Criação de um modelo de usuário com campos para nome de usuário e senha.
    - Implementação de views para login e cadastro de usuários.
    - Validação de senha e nome de usuário durante o cadastro.
    - Utilização de hash para armazenar senhas de forma segura.
    - Adição de mensagens de feedback para o usuário em caso de sucesso ou erro.
    - Redirecionamento para a página inicial após o login bem-sucedido.

--- coffe_app/core/views.py
+++ coffe_app/core/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password, check_password
+from .models import usuario
 
-#### login ###
+### LOGIN ###
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
+        try:
+            user = usuario.objects.get(nome=username)
+            if check_password(password, user.senha):
+                request.session['user_id'] = user.id
+                return redirect('home')
+            else:
+                messages.error(request, 'Usuário ou senha inválidos')
+        except usuario.DoesNotExist:
+            messages.error(request, 'Usuário ou senha inválidos')
 
-def login_view(request): 
     return render(request, 'pages/login/login.html')
 
 
+### CADASTRO ###
 def cadastro_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('confirm_password')
+
+        if password != password_confirm:
+            messages.error(request, 'As senhas não coincidem')
+            return render(request, 'pages/login/cadastro.html')
+
+        if usuario.objects.filter(nome=username).exists():
+            messages.error(request, 'Usuário já existe')
+            return render(request, 'pages/login/cadastro.html')
+
+        usuario.objects.create(nome=username, senha=make_password(password))
+        messages.success(request, 'Usuário cadastrado com sucesso')
+        return redirect('login')
+
     return render(request, 'pages/login/cadastro.html')

51568a0aac1dad06fbf1ecf18c15251bd3c147d2
Lucas Guerra <125227136+eolkazin@users.noreply.github.com>
2025-05-08 21:49:37 -0300

    criação tela de cadastro e rota entre login e cadastro

--- coffe_app/coffe_app/urls.py
+++ coffe_app/coffe_app/urls.py
@@ -4,4 +4,5 @@ from core import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
+    path('cadastro/', views.cadastro_view, name='cadastro'),
 ]
--- coffe_app/core/views.py
+++ coffe_app/core/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render , redirect
 #### login ###
 
 
-def login_view(request):
+def login_view(request): 
     return render(request, 'pages/login/login.html')
+
+
+def cadastro_view(request):
+    return render(request, 'pages/login/cadastro.html')

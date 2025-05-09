f75cd262884e3b1dd555e82801a167fd13326f6a
Lucas Guerra <125227136+eolkazin@users.noreply.github.com>
2025-05-08 22:34:41 -0300

    feat: Implementa autenticação e cadastro de usuários
    Implementa a lógica de autenticação e cadastro de usuários, incluindo:
    
    - Criação de um modelo de usuário com campos para nome de usuário e senha.
    - Implementação de views para login e cadastro de usuários.
    - Validação de senha e nome de usuário durante o cadastro.
    - Utilização de hash para armazenar senhas de forma segura.
    - Adição de mensagens de feedback para o usuário em caso de sucesso ou erro.
    - Redirecionamento para a página inicial após o login bem-sucedido.

--- coffe_app/core/models.py
+++ coffe_app/core/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-
-# === BANCO DE DADOS ===
 class usuario(models.Model):
-    nome = models.CharField(max_length=255)
+    nome = models.CharField(max_length=255, unique=True)
     senha = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nome
+
+    class Meta:
+        db_table = 'usuarios'
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'

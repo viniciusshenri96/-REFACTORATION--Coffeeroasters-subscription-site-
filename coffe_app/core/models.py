320e90b7926f7cc85388a652661f478a78b9bf88
Lucas Guerra <125227136+eolkazin@users.noreply.github.com>
2025-05-08 21:56:44 -0300

    criação da tabela de login SQLITE

--- coffe_app/core/models.py
+++ coffe_app/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# === BANCO DE DADOS ===
+class usuario(models.Model):
+    nome = models.CharField(max_length=255)
+    senha = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
